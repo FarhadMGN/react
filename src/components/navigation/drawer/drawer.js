4c717d9c313b302a3dce371d67d88731b9377d27
Farhad74 <evgeniy_mgn_74@mail.ru>
2021-09-18 16:08:38 +0300

    menu is added

---
+++ src/components/navigation/drawer/drawer.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react'
+import classes from './drawer.module.css'
+import Backdrop from "../../UI/backdrop/backdrop";
+
+const links = [
+    1, 2, 3
+];
+
+class Drawer extends Component {
+
+    renderLinks() {
+        return links.map((link, idx) => {
+            return (
+                <li key={idx}>
+                    <a>Link {link}</a>
+                </li>
+            )
+        })
+    }
+
+    render() {
+        const cls = [
+            classes.Drawer
+        ];
+
+        if (!this.props.isOpen) {
+            cls.push(classes.close);
+        }
+        return (
+            <React.Fragment>
+                <nav className={cls.join(' ')}>
+                    <ul>
+                        { this.renderLinks() }
+                    </ul>
+                </nav>
+                { this.props.isOpen ? <Backdrop onClick={this.props.onClose}/> : null }
+            </React.Fragment>
+        )
+    }
+}
+
+export default Drawer;
--- src/hoc/layout/layout.js
+++ src/hoc/layout/layout.js
@@ -1,10 +1,35 @@
 import React, {Component} from 'react'
 import classes from './layout.module.css'
+import MenuToggle from "../../components/navigation/menu-toggle/menu-toggle";
+import Drawer from "../../components/navigation/drawer/drawer";
 
 class Layout extends Component {
+    state = {
+        menu: false,
+    };
+
+    menuHandler = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    };
+    menuCloseHandler = () => {
+        this.setState({
+            menu: false
+        })
+    };
     render() {
         return (
             <div className={classes.Layout}>
+                <Drawer
+                    isOpen={this.state.menu}
+                    onClose={this.menuCloseHandler}
+                />
+                <MenuToggle
+                    onToggle={this.menuHandler}
+                    isOpen={this.state.menu}
+                />
+
                 <main>
                     { this.props.children }
                 </main>
